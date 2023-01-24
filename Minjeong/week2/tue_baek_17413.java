20cc9f0cad6693883735211c1708e52993550ca0
권민정 <22alswjd@naver.com>
2023-01-24 11:20:26 +0900

    refactor: baek no.17413 불필요한 루프 제거

--- Minjeong/week2/tue_baek_17413.java
+++ Minjeong/week2/tue_baek_17413.java
@@ -6,36 +6,31 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder res = new StringBuilder();
+        StringBuilder word = new StringBuilder();
         String s = br.readLine();
         int length = s.length();
+
         for (int i = 0; i < length; i++) {
-            StringBuilder word = new StringBuilder();
-            for (int j = i; j < length; j++) {
-                char c = s.charAt(j);
-                if (c == '<') {
+            char c = s.charAt(i);
+            if (c == '<') {
+                word.reverse();
+                res.append(word);
+                word.setLength(0);
+                int j = i;
+                while (s.charAt(j) != '>') j++;
+                res.append(s, i, j + 1);
+                i = j;
+            }
+            else if (c == ' ') {
+                word.reverse();
+                res.append(word).append(' ');
+                word.setLength(0);
+            }
+            else if (c >= '0' && c <= '9' || c >= 'a' && c <= 'z') {
+                word.append(c);
+                if (i == length - 1) {
                     word.reverse();
                     res.append(word);
-                    word.setLength(0);
-                    int k = j;
-                    while (s.charAt(k) != '>') k++;
-                    res.append(s, j, k + 1);
-                    i = k;
-                    break;
-                }
-                else if (c == ' ') {
-                    word.reverse();
-                    res.append(word).append(' ');
-                    word.setLength(0);
-                    i = j;
-                    break;
-                }
-                else if (c >= '0' && c <= '9' || c >= 'a' && c <= 'z') {
-                    word.append(c);
-                    if (j == length - 1) {
-                        word.reverse();
-                        res.append(word);
-                        i = length - 1;
-                    }
                 }
             }
         }
