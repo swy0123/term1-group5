ec06bd96c615ab892649c380ed57f1a50dd50ddd
JJewon <34835912+eldertree777@users.noreply.github.com>
2023-01-28 01:27:44 +0900

    BEAK_2304

--- Jewon/Problem/week2/BEAK_2304.java
+++ Jewon/Problem/week2/BEAK_2304.java
@@ -1,14 +1,8 @@
 package week2;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  * 창고 다각형 2304번 Silver 2
@@ -21,32 +15,53 @@ public class BEAK_2304 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		Map<Integer, Integer> hMap = new HashMap<>();
 
 		int N = sc.nextInt();
+		Integer[][] list = new Integer[N][2];
+
 		for (int i = 0; i < N; i++) {
-			hMap.put(sc.nextInt(), sc.nextInt());
+			list[i][0] = sc.nextInt();
+			list[i][1] = sc.nextInt();
 		}
-		
-		List<Entry<Integer,Integer>> list = new ArrayList<>();
-		
-		for(Entry<Integer,Integer> entryset : hMap.entrySet()) {
-			list.add(entryset);
+
+		Arrays.sort(list, new Comparator<Integer[]>() {
+
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				// TODO Auto-generated method stub
+				return Integer.compare(o1[0], o2[0]); // x - y > 0 이면 교환
+			}
+
+		});
+
+		int maxval = 0, maxidx = 0;
+		for (int i = 0; i < list.length; i++) {
+			if (list[i][1] > maxval) {
+				maxval = list[i][1];
+				maxidx = i;
+			}
 		}
-		
-//		Set<Entry<Integer, Integer>> entryList = hMap.entrySet();
 
-//		Collections.sort(entryList, new Comparator<Entry<Integer, Integer>>() {
-//
-//			@Override
-//			public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-//				// TODO Auto-generated method stub
-//				return Integer.compare(o1.getKey(), o2.getKey());
-//			}
-//
-//		});
+		int sum = 0, currentmaxidx = 0;
+		for (int i = 1; i <= maxidx; i++) {
+			if (list[i][1] >= list[currentmaxidx][1]) {
+				sum += (list[i][0] - list[currentmaxidx][0]) * list[currentmaxidx][1];
+				currentmaxidx = i;
+			}
+		}
 
-		//System.out.println(entryList.toString());
+		currentmaxidx = list.length - 1;
+		for (int i = currentmaxidx - 1; i >= maxidx; i--) {
+			if (list[i][1] >= list[currentmaxidx][1]) {
+				sum += (list[currentmaxidx][0] - list[i][0]) * list[currentmaxidx][1];
+				currentmaxidx = i;
+			}
+		}
+
+		sum += maxval;
+
+		System.out.println(sum);
+
 	}
 
 }
