a65d49fb02d03d8b79f3b7d6221cfd93e0e8cc3b
이시영 <61828877+swy0123@users.noreply.github.com>
2023-01-27 16:45:43 +0900

    백준 [골드5] 14719 : 창고다각형

---
+++ Siyoung/week2/fri_baek_14719.java
@@ -0,0 +1,49 @@
+package assign;
+
+import java.util.Scanner;
+/**
+ * 백준 [골드5] 14719 : 창고다각형
+ * @author SSAFY
+ *
+ */
+public class fri_baek_14719 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int w = sc.nextInt();
+		
+		int[] arr = new int[w];
+		
+		int max = 0;
+		int maxIdx = 0;
+		int sum = 0;
+		
+		for(int i=0; i<w; i++) {
+			arr[i] = sc.nextInt();
+			sum -= arr[i];
+			if(arr[i] > max) {
+				max = arr[i];
+				maxIdx = i;
+			}
+		}
+		int cur = 0;
+		for(int i=0; i<maxIdx; i++) {
+			if(arr[i]>cur) {
+				cur = arr[i];
+			}
+			sum+=cur;
+		}
+		cur = 0;
+		for(int i=w-1; i>=maxIdx; i--) {
+			if(arr[i]>cur) {
+				cur = arr[i];
+			}
+			sum+=cur;
+		}
+		System.out.println(sum);
+	}
+
+}
