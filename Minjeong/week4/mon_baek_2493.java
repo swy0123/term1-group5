5223bab004859adfaf6a84b842c8720282bb6d40
minpaeng <22alswjd@naver.com>
2023-02-07 10:49:11 +0900

    baek no.2493

---
+++ Minjeong/week4/mon_baek_2493.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+class Node {
+	int idx;
+	int val;
+	
+	public Node(int idx, int val) {
+		this.idx = idx;
+		this.val = val;
+	}
+}
+
+public class Main {
+    private static Stack<Node> s1 = new Stack<>();
+    private static Stack<Node> s2 = new Stack<>();
+    private static int n;
+    private static int[] res;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        res = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+        	int a = Integer.parseInt(st.nextToken());
+        	s1.push(new Node(i, a));
+        }
+        
+        boolean flag = false;
+        while (!s1.empty()) {
+        	Node cur;
+        	if (s2.empty()) {
+        		cur = s1.pop();
+        		s2.push(cur);
+        	}
+        	else cur = s2.peek();
+        	
+        	if (s1.empty()) break;
+        	
+        	if (s1.peek().val >= cur.val) {
+        		flag = true;
+        		res[cur.idx] = s1.peek().idx + 1;
+        		s2.pop();
+        	}
+        	else {
+        		s2.push(s1.peek());
+        		s1.pop();
+        	}
+        }
+        
+        if (!flag) System.out.println(0);
+        else {
+        	for (int i = 0; i < n; i++) {
+        		System.out.print(res[i] + " ");
+        	}
+        }
+    }
+}
