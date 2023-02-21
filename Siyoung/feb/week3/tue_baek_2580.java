69f8412b9fc912b8a4c4e649c45933c92b239018
이시영 <61828877+swy0123@users.noreply.github.com>
2023-02-21 13:36:21 +0900

    백준 2580 스도쿠

--- Siyoung/feb/week3/tue_baek_2580.java
+++ Siyoung/feb/week3/tue_baek_2580.java
@@ -8,12 +8,12 @@ import java.util.StringTokenizer;
  * 백준 2580 스도쿠
  */
 public class tue_baek_2580 {
-//	static int[][] map;
+	static int[][] map;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int[][] map = new int[9][9];
+		map = new int[9][9];
 		int zero = 0;
 		for(int i=0; i<9; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -23,10 +23,10 @@ public class tue_baek_2580 {
 			}
 		}
 		
-		sdoku(map, zero);
+		sdoku(zero);
 	}
 	
-	private static void sdoku(int[][] map, int zero) {
+	private static void sdoku(int zero) {
 		if(zero == 0) {
 			for (int[] is : map) {
 				for (int is2 : is) {
@@ -37,16 +37,11 @@ public class tue_baek_2580 {
 			System.exit(0);
 			return;
 		}
-		int[][] tmp = new int[9][9];
-		for(int i=0; i<9; i++) {
-			tmp[i] = map[i].clone();
-		}
-		
 		int ci=-1, cj=-1;
 		
 		label : for(int i=0; i<9; i++) {
 			for(int j=0; j<9; j++) {
-				if(tmp[i][j] == 0) {
+				if(map[i][j] == 0) {
 					ci = i;
 					cj = j;
 					break label;
@@ -55,38 +50,30 @@ public class tue_baek_2580 {
 		}
 		
 		for(int k=1; k<=9; k++) {
-			tmp[ci][cj] = k;
-			if(check(tmp, ci, cj)) {
-				sdoku(tmp, zero-1);
+			if(check(ci, cj, k)) {
+				map[ci][cj] = k;
+				sdoku(zero-1);
+				map[ci][cj] = 0;
 			}
 			
 		}
 	}
 	//가로 세로 3*3체크
-	private static boolean check(int[][] map, int r, int c) {
-		int[] arr1 = new int[9];
-		int[] arr2 = new int[9];
-		int[] arr3 = new int[9];
+	private static boolean check(int r, int c, int num) {
 		//세로줄
 		for(int i=0; i<9; i++) {
-			if(map[i][c] == 0) continue;
-			arr1[map[i][c]-1]+=1;
-			if(arr1[map[i][c]-1]>1)return false;
+			if(map[i][c] == num) return false;
 		}
 		
 		//가로줄
 		for(int i=0; i<9; i++) {
-			if(map[r][i] == 0) continue;
-			arr2[map[r][i]-1]+=1;
-			if(arr2[map[r][i]-1]>1)return false;
+			if(map[r][i] == num) return false;
 		}
 		
 		//3*3
 		for(int i=(r/3)*3; i<(r/3)*3+3; i++) {
 			for(int j=(c/3)*3; j<(c/3)*3+3; j++) {
-				if(map[i][j] == 0) continue;
-				arr3[map[i][j]-1]+=1;
-				if(arr3[map[i][j]-1]>1)return false;
+				if(map[i][j] == num) return false;
 			}
 		}
 
