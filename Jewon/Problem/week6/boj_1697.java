36e302fb510971248cf9514111c5b228aa027b3a
JJewon <34835912+eldertree777@users.noreply.github.com>
2023-02-23 20:03:40 +0900

    boj_1697 숨바꼭질 실버 1
    + 자기 자리 확인 (N == M)
    + map 배열의 초기 값이 0 이지만 N,M 입력값이 0을 포함하기에 문제 발생

--- Jewon/Problem/week6/boj_1697.java
+++ Jewon/Problem/week6/boj_1697.java
@@ -1,8 +1,9 @@
-package solving;
+package week6;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -24,14 +25,21 @@ public class boj_1697 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-
+		int ans = 0;
+		Arrays.fill(map, -1);
 		map[N] = N;
 		map[M] = M;
-
-		bfs(N);
+		
+		if (N == M) {
+			ans = 0;
+		} else {
+			ans = bfs(N);
+		}
+		
+		System.out.println(ans);
 	}
 
-	private static void bfs(int n) {
+	private static int bfs(int n) {
 		Queue<Integer> q = new LinkedList<Integer>();
 		q.offer(n);
 
@@ -45,21 +53,39 @@ public class boj_1697 {
 
 				// x-1
 				if (num - 1 >= 0 && map[num - 1] != N) {
+					if (map[num - 1] == M) {
+						return level + 1;
+					}
 
+					q.offer(num - 1);
+					map[num - 1] = N;
 				}
 
 				// x+1
-				if (map[num +1] != N) {
+				if (num + 1 < 100_001 && map[num + 1] != N) {
+					if (map[num + 1] == M) {
+						return level + 1;
+					}
 
+					q.offer(num + 1);
+					map[num + 1] = N;
 				}
 
 				// x*2
-				if (map[num * 2] != N) {
+				if (num * 2 < 100_001 && map[num * 2] != N) {
+					if (map[num * 2] == M) {
+						return level + 1;
+					}
 
-					
-
+					q.offer(num * 2);
+					map[num * 2] = N;
+				}
 			}
+//			System.out.println(Arrays.toString(q.toArray()));
+			level++;
 		}
-	}
 
+		return -1;
+
+	}
 }
