31282274963b59ef2b02ddfa20f6ae889fe75774
JJewon <34835912+eldertree777@users.noreply.github.com>
2023-03-18 17:49:14 +0900

    boj_19238 스타트 택시 골드2

--- Jewon/Problem/week9/boj_19238.java
+++ Jewon/Problem/week9/boj_19238.java
@@ -1,9 +1,8 @@
-package week9;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -57,7 +56,7 @@ public class boj_19238 {
 	static int[][] map;
 	static Point My_car;
 	static List<customer> customers;
-	static boolean[] customers_status;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -65,6 +64,7 @@ public class boj_19238 {
 		M = Integer.parseInt(st.nextToken());
 		fuel = Integer.parseInt(st.nextToken());
 		map = new int[N][N];
+		customers = new ArrayList<>();
 
 		for (int i = 0; i < map.length; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -78,8 +78,7 @@ public class boj_19238 {
 		int row = Integer.parseInt(st.nextToken()) - 1;
 		int col = Integer.parseInt(st.nextToken()) - 1;
 		My_car = new Point(row, col);
-		customers_status = new boolean[M];
-		
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int c_row = Integer.parseInt(st.nextToken()) - 1;
@@ -89,33 +88,45 @@ public class boj_19238 {
 
 			customers.add(new customer(new Point(c_row, c_col), new Point(t_row, t_col)));
 		}
-		
+
 		// find 가까운 친구 고르기
-		while (customers.isEmpty()) {
-			if(work()) {
-				// -1
+		while (!customers.isEmpty()) {
+//			System.out.println(fuel);
+//			System.out.print(Arrays.toString(customers.toArray()) + " ");
+//			System.out.println(My_car);
+			if (work()) {
+				System.out.println(-1);
+				System.exit(0);
 			}
 		}
 		// 고객으로 이동 -> 목적지로 이동
-
+		System.out.println(fuel);
 	}
 
 	private static boolean work() {
-		// 현재 서있는곳에 고객이 있나?
 		
 		// 가까운 승객 탐색
-		Boolean[][] visited = new Boolean[N][N];
+		boolean[][] visited = new boolean[N][N];
 		Queue<Point> q = new LinkedList<Point>();
 		visited[My_car.row][My_car.col] = true;
 		q.offer(My_car);
 
 		List<customer> client_now = new ArrayList<>();
+		// 현재 서있는곳에 고객이 있나?
+		for (int i = 0; i < customers.size(); i++) {
+			if(My_car.row == customers.get(i).loc.row &&
+					My_car.col == customers.get(i).loc.col) {
+				client_now.add(customers.get(i));
+				break;
+			}
+		}
 		
+
 		int size = 0;
 		int level = 0;
 		while (!q.isEmpty()) {
 			size = q.size();
-			if(client_now.size() > 0) {
+			if (client_now.size() > 0) {
 				break;
 			}
 			for (int i = 0; i < size; i++) {
@@ -126,73 +137,121 @@ public class boj_19238 {
 					int nc = p.col + dir[d][1];
 
 					if (nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] != 1) {
-						if(!visited[nr][nc]) {
-							for(int c = 0; c < customers.size(); c++ ) {
-								if(!customers_status[c]) {
-									if(nr == customers.get(c).loc.row && nc == customers.get(c).loc.col) {
-										client_now.add(customers.get(c));
-									}
+						if (!visited[nr][nc]) {
+							for (int c = 0; c < customers.size(); c++) {
+
+								if (nr == customers.get(c).loc.row && nc == customers.get(c).loc.col) {
+									client_now.add(customers.get(c));
 								}
+
 							}
-							
+
 							visited[nr][nc] = true;
-							q.offer(new Point(nr,nc));							
+							q.offer(new Point(nr, nc));
 						}
 					}
 				}
 			}
 			level++;
 		}
-		
+
 		Point Hello_customer = null;
 		Point Hello_Target = null;
 		// 고객 정하기
 		int idx = -1;
-		if(client_now.size() == 0) {
+		if (client_now.size() == 0) {
 			return true;
-		}else if(client_now.size() == 1) {
+		} else if (client_now.size() == 1) {
 			Hello_customer = client_now.get(0).loc;
 			Hello_Target = client_now.get(0).target;
-		}else if(client_now.size() > 1) {
+		} else if (client_now.size() > 1) {
 			Point current_loc = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
 			for (int i = 0; i < client_now.size(); i++) {
-				if(current_loc.row > client_now.get(i).loc.row) {
+				if (current_loc.row > client_now.get(i).loc.row) {
 					idx = i;
 					current_loc = client_now.get(i).loc;
-				}else if(current_loc.row == client_now.get(i).loc.row) {
-					if(current_loc.col > client_now.get(i).loc.col) {
+				} else if (current_loc.row == client_now.get(i).loc.row) {
+					if (current_loc.col > client_now.get(i).loc.col) {
 						idx = i;
 						current_loc = client_now.get(i).loc;
 					}
 				}
 			}
-			
+
 			Hello_customer = current_loc;
 			Hello_Target = client_now.get(idx).target;
 		}
-		
-		
+
 		//
 		fuel -= level;
-		if(fuel < 0) {
+		if (fuel < 0) {
 			return true;
 		}
-		
-		//목적지로 이동하기
-		visited = new Boolean[N][N];
+
+		// 목적지로 이동하기
+		visited = new boolean[N][N];
 		q = new LinkedList<Point>();
 		visited[Hello_customer.row][Hello_customer.col] = true;
 		q.offer(new Point(Hello_customer.row, Hello_customer.col));
-		
+
 		level = 0;
-		while(!q.isEmpty()) {
+		boolean flag = false;
+
+		outer: while (!q.isEmpty()) {
 			size = q.size();
 			for (int i = 0; i < size; i++) {
 				Point p = q.poll();
+
+				if (p.row == Hello_Target.row && p.col == Hello_Target.col) {
+					flag = true;
+					break outer;
+				}
+
+				for (int j = 0; j < dir.length; j++) {
+					int nr = p.row + dir[j][0];
+					int nc = p.col + dir[j][1];
+
+					if (nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] != 1) {
+						if (!visited[nr][nc]) {
+							q.offer(new Point(nr, nc));
+							visited[nr][nc] = true;
+						}
+					}
+				}
+			}
+
+			level++;
+		}
+
+		// 연료 계산하기, my_car 업데이트
+		if (flag) { // 연료 상관없이 도착 못하는 경우
+			// 목적지가 도착 가능?
+			fuel -= level;
+			if (fuel < 0) {
+				return true;
+			}
+
+			// 연료 충전
+			fuel += level * 2;
+		} else {
+			return true;
+		}
+
+		// 고객 리스트 제외
+		for (int i = 0; i < customers.size(); i++) {
+			if (Hello_customer.row == customers.get(i).loc.row && Hello_customer.col == customers.get(i).loc.col) {
+				customers.remove(i);
+				break;
 			}
 		}
 		
-		// 연료 계산하기, my_car 업데이트
+		
+		// 차량 위치 업데이트
+		My_car.row = Hello_Target.row;
+		My_car.col = Hello_Target.col;
+		
+		return false;
+
 	}
 
 }
