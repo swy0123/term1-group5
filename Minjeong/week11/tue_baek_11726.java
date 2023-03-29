cd54971e2d4386ced595fefb71b4214baff5ec33
권민정 <22alswjd@naver.com>
2023-03-30 00:22:04 +0900

    baek_11726_2xn 타일링

---
+++ Minjeong/week11/tue_baek_11726.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 2xn 타일링
+public class tue_baek_11726 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] res = new int[n + 1];
+        res[1] = 1;
+        if (n >= 2) res[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            res[i] = (res[i - 1] + res[i - 2]) % 10007;
+        }
+
+        System.out.println(res[n]);
+    }
+}
