e31b6267bce2e357bcd7a1fe1f61e56116ae52b8
이시영 <swy05123@gmail.com>
2023-04-03 23:49:53 +0900

    2023-04-03_2

--- Siyoung/Main.java
+++ Siyoung/Main.java
@@ -1,66 +1,138 @@
-package test;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class node{
-		int i, j;
-		public node(int i, int j) {
-			super();
-			this.i = i;
-			this.j = j;
-		}
-	}
-	static int n, map[][], dp[][];
+	
+	static int[] di = {-1, 1, 0, 0};
+	static int[] dj = {0, 0, -1, 1};
+	
+	static int n, m, hi, hj, res;
+	static char[][] map;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		map = new char[n][m];
+		hi = -1;
+		hj = -1;
 		
-		map = new int[n][n];
 		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<n; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+			String str = br.readLine();
+			for(int j=0; j<m; j++) {
+				map[i][j] = str.charAt(j);
+				if(map[i][j] == 'O') {
+					hi = i;
+					hj = j;
+				}
+			}
+		}
+		res = 11;
+		solve(0, 5);
+		System.out.println(res);
+	}
+	
+	//백트래킹
+	private static void solve(int cnt, int dir) {
+		if(cnt == 10) {
+			for (char[] is : map) {
+				System.out.println(Arrays.toString(map));
+			};
+			return;
+		}
+		
+		int ri=0, rj=0, bi=0, bj=0;
+		char[][] tmp = new char[n][m];
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				tmp[i][j] = map[i][j];
+				if(map[i][j]=='R') {
+					ri = i;
+					rj = j;
+				}
+				if(map[i][j]=='B') {
+					bi = i;
+					bj = j;
+				}
 			}
 		}
 		
-		dp = new int[n][n];
-//		dp[0][0] = 1;
-//		for(int i=0; i<n; i++) {
-//			for(int j=0; j<n; j++) {
-//				int ni = i+1;
-//				int nj = j+1;
-//				if(ni<n) {
-//					if(map[ni][j]>map[i][j]) dp[ni][j] = Math.max(dp[ni][j], dp[i][j]+1);
-//					else dp[ni][j] = dp[i][j];
-//				}
-//				if(nj<n) {
-//					if(map[i][nj]>map[i][j]) dp[i][nj] = Math.max(dp[i][nj], dp[i][j]+1);
-//					else dp[i][nj] = dp[i][j];
-//				}
-//			}
-//		}
-//		for (int[] is : dp) {
-//			System.out.println(Arrays.toString(is));
-//		}
-		
-		System.out.println(dp[n-1][n-1]);
-	}
-	
-	private static int solve(node cn) {
-		if(cn.i==0 && cn.j==0) {
-			return 1;
+		for(int d=0; d<4; d++) {
+			if(d == dir) continue;
+			
+			int state = moveBall(ri, rj, bi, bj, d);
+			if(state==0) continue;
+			if(state==1) res = Math.min(state, res);
+			if(state==2) return;
+			if(state==3) return;
+			solve(cnt+1, d);
+			for(int i=0; i<n; i++) {
+				map[i] = tmp[i].clone();
+			}
 		}
 		
+	}
+	
+	private static int moveBall(int ri, int rj, int bi, int bj, int d) {
 		
+		int cri = ri, crj = rj, cbi = bi, cbj = bj;
+		boolean redCheck = false, blueCheck = false;
+		boolean redGo = true, blueGo = true;
+		while(redGo || blueGo) {
+			int nri = cri+di[d];
+			int nrj = crj+di[d];
+			if(nri>0 && nri<n-1 && nrj>0 && nrj<m-1) {
+				if(map[nri][nrj] == 'O') {
+					map[cri][crj] = '.';
+					redCheck = true;
+					redGo = false;
+				}
+				else if(map[nri][nrj] == '.') {
+					map[cri][crj] = '.';
+					map[nri][nrj] = 'R';
+					cri = nri;
+					crj = nrj;
+					redGo = true;
+				}
+				else {
+					redGo = false;
+				}
+			}
+			
+
+			int nbi = cbi+di[d];
+			int nbj = cbj+di[d];
+			if(nbi>0 && nbi<n-1 && nbj>0 && nbj<m-1) {
+				if(map[nbi][nbj] == 'O') {
+					map[cbi][cbj] = '.';
+					blueCheck = true;
+					blueGo = false;
+				}
+				else if(map[nbi][nbj] == '.') {
+					map[cbi][cbj] = '.';
+					map[nbi][nbj] = 'B';
+					cbi = nbi;
+					cbj = nbj;
+					blueGo = true;
+				}
+				else {
+					blueGo = false;
+				}
+			}
+		}
+		
+		if(redCheck && blueCheck) return 3;
+		if(!redCheck && blueCheck) return 2;
+		if(redCheck && !blueCheck) return 1;
 		return 0;
-		
 	}
 }
+
